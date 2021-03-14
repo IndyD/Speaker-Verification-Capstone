bd85d97fae6c6faa8744324400bc9a962f364d66
Indy Dhillon <indy.dhillon1@gmail.com>
2021-03-13 17:02:50 -0800

    Added VoxCeleb download script

---
+++ downloadVoxCeleb2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## download files sequentially
+wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox2_dev_mp4_partaa &&
+wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox2_dev_mp4_partab &&
+wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox2_dev_mp4_partac &&
+wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox2_dev_mp4_partad &&
+wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox2_dev_mp4_partae &&
+wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox2_dev_mp4_partaf &&
+wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox2_dev_mp4_partag &&
+wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox2_dev_mp4_partah &&
+wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox2_dev_mp4_partai &&
+wget https://thor.robots.ox.ac.uk/~vgg/data/voxceleb/vox1a/vox2_test_mp4.zip 
+
+## gather data
+cat vox2_dev_aac* > vox2_aac.zip
+mkdir data
+mv vox2_aac.zip data
+unzip data/vox2_aac.zip
+rm vox2_dev_aac*
