f36dc90a5bbeb2fcee606633b78ce8af9f681967
Indy Dhillon <indy.dhillon1@gmail.com>
2021-04-17 23:04:51 -0700

    Tuned spectogram based on vggvox/librosa recommendations

--- spect_test.py
+++ spect_test.py
@@ -2,11 +2,21 @@ import librosa
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa.display
+import pdb
+import sys
 
-fname = 'test_utterance.wav'
+fname = sys.argv[1]
 y,sr = librosa.load(fname)
-S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+S = librosa.feature.melspectrogram(
+    y, sr=sr, 
+    n_fft=512, 
+    n_mels=130, 
+    win_length=512, 
+    hop_length=222,
+)
 log_S = librosa.power_to_db(S, ref=np.max)
+
+pdb.set_trace()
 
 plt.figure(figsize=(12,4))
 librosa.display.specshow(log_S, sr=sr, x_axis='time', y_axis='mel')
